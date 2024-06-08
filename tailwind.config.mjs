acaec2a2a111db32aeaae19fd8c43c6cc2a860ec
Joey Farruggio <joey@joeyfarruggio.com>
2024-06-08 13:17:31 -0500

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			sans: ['Outfit', 'Inter', 'sans-serif'],
-			cursive: ['Pacifico', 'cursive'],
+			sans: ["Outfit", "Inter", "sans-serif"],
+			cursive: ["Pacifico", "cursive"],
 		},
-		extend: {},
+		extend: {
+			screens: {
+				xl: "1320px",
+			},
+		},
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+	plugins: [require("@tailwindcss/typography")],
+};
