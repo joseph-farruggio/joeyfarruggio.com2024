24a0aaa5b64d3289989d458d3c4d206fdc56d1c9
Joey Farruggio <joey@joeyfarruggio.com>
2024-06-10 13:55:52 -0500

    menu updates

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,6 +5,26 @@ import intersect from "@alpinejs/intersect";
 Alpine.plugin(intersect);
 Alpine.plugin(focus);
 
+Alpine.data("menu", () => ({
+	open: false,
+	toggle() {
+		if (this.open) {
+			return this.close();
+		}
+
+		this.$refs.button.focus();
+
+		this.open = true;
+	},
+	close(focusAfter) {
+		if (!this.open) return;
+
+		this.open = false;
+
+		focusAfter && focusAfter.focus();
+	},
+}));
+
 Alpine.data("stars", () => ({
 	init() {
 		setInterval(() => {
